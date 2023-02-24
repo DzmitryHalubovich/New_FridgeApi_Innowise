397fa690493bc88a2af362093ac08897e8a67ca8
DzmitryHalubovich <buxarik493@gmail.com>
2023-02-24 19:05:08 +0300

    add auth controller

---
+++ src/FridgeApi_Innowise/Models/UserDto.cs
@@ -0,0 +1,9 @@
+﻿namespace FridgeApi_Innowise.Models
+{
+    public class UserDto
+    {
+        public required string Username { get; set; } = string.Empty;
+
+        public required string Password { get; set; } = string.Empty;
+    }
+}
